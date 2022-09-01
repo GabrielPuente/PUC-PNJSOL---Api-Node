840feb82c00576ae3b5731f703dbc209694035d1
Gabriel Puente <gapuente96@gmail.com>
2022-09-01 20:03:26 -0300

    rota inicial

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) =>{
+    res.send(`Aplicação rodando. <br/><br/>
+
+    Rotas: /products<br/><br/>
+    Modelo - Product <br/>
+    Campos:<br/><br/>
+    id: int,<br/>
+    description: string,<br/>
+    value: float,<br/>
+    brand: string`);
+});
+
 require('./controllers/productController')(app);
 
 app.listen(port);
