019b89886f37b7e13ceee6a416a28a604d7ea884
Gabriel Puente <gapuente96@gmail.com>
2022-09-19 20:45:10 -0300

    integrado ao banco de dados

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.send(`Aplicação rodando. <br/><br/>
 
     Rotas: /products<br/><br/>
